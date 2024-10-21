d703181576a2d5d4d269ded231fb8d4639b03a83
Braison Wabwire <braisonwabwire2003@gmail.com>
2024-10-21 16:52:13 +0300

    react states introduction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
+import Welcome from './components/welcome';
+import Message from './components/message';
 
 function App() {
   return (
-    <div className="App">
-      <Greet name='Braison' heroname='Tajiri'>
-        <h1>This is childrenprops</h1>
-        </Greet>
-      <Greet name='Wabwire' heroname='mkuu'/>
-      <Greet name='Daniel' heroname='ule msee'/>
-    </div>
+    // <div className="App">
+    //   <Greet name='Braison' heroname='Tajiri'>
+    //     <h1>This is childrenprops</h1>
+    //     </Greet>
+    //   <Greet name='Wabwire' heroname='mkuu'>
+    //     <button > Action </button>
+    //   </Greet>
+    //   <Greet name='Daniel' heroname='ule msee'/>
+    //   <Welcome name='James' heroname='Mnoma'></Welcome>
+    // </div>
+    <Message></Message>
+
   );
 }
 
