dcd3818942c7366c5ec3c24ff904eed63b0c27e0
Braison Wabwire <braisonwabwire2003@gmail.com>
2024-10-24 21:49:10 +0300

    Intro to Event handling in React

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import Greet from './components/greet';
 import Welcome from './components/welcome';
 import Message from './components/message';
 import Counter from './components/counter';
+import FunctionClick from './components/functionClick';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet name='Braison' heroname='Tajiri'>
-        <h1>This is childrenprops</h1> 
-      </Greet> */}
-      {/* <Greet name='Wabwire' heroname='mkuu'>
-        <button > Action </button>
-      </Greet> */}
-      {/* <Greet name='Daniel' heroname='ule msee' /> */}
-      <Welcome name='James' heroname='Mnoma'></Welcome>
+      <FunctionClick></FunctionClick>
     </div>
-    // <Message></Message>
-    // <Counter></Counter>
-
   );
 }
 
